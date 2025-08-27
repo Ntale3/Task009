de21cac0e99553439b5b2c9a1a47f51a3d126e5b
Ntale Samad <swamaduntale98@gmail.com>
2025-08-27 19:10:54 +0300

    add package-info to respective modules

---
+++ PostgresConn/src/main/java/org/database/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides utility classes for managing PostgreSQL database connections
+ * and performing CRUD operations such as inserting, updating,
+ * deleting, and retrieving user data.
+ *
+ * <p>This package is mainly used by the application to abstract
+ * database interactions and simplify working with JDBC.</p>
+ */
+
+package org.database;
+
---
+++ UIManager/src/main/java/org/ui/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * User interface components for the authentication system.
+ * This package contains classes for managing login, registration,
+ * and dashboard UI components using Swing framework.
+ *
+ * <p>Main classes include:
+ * <ul>
+ * <li>{@link org.ui.AuthUI} - Main authentication UI controller</li>
+ * <li>{@link org.ui.LogIn} - Login form components</li>
+ * <li>{@link org.ui.SignUp} - Registration form components</li>
+ * <li>{@link org.ui.DashBoard} - Dashboard interface components</li>
+ * <li>{@link org.ui.Component} - Utility class for UI component creation</li>
+ * </ul>
+ *
+ * @since 1.0
+ * @version 1.0-SNAPSHOT
+ */
+package org.ui;
